b187dd68c280c121e480cd151b2a1aa8eaf944a4
sopanda <hushchabogdan@gmail.com>
2018-10-14 20:50:32 +0300

    temporary disable auth in some controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
-  before_action :authenticate_user, except: %i[create]
+  #before_action :authenticate_user, except: %i[create]
 
   def index
     @users = User.all
