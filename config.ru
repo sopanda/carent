b828180f8f4af2b57ef66846cc026d878fc24304
Bohdan <hushchabogdan@gmail.com>
2019-02-10 15:40:04 +0100

    cors configuration

--- config.ru
+++ config.ru
@@ -5,3 +5,15 @@
 require_relative 'config/environment'
 
 run Rails.application
+
+require 'rack/cors'
+use Rack::Cors do
+
+  # allow all origins in development
+  allow do
+    origins '*'
+    resource '*', 
+        :headers => :any, 
+        :methods => [:get, :post, :delete, :put, :options]
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,12 @@ module Carent
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += [Rails.root.join('app', 'validators').to_s]
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
