a57ae6f339d2de0589d536d0f9fc216a97ecf388
sopanda <hushchabogdan@gmail.com>
2019-02-11 21:31:17 +0200

    added latitude and longitude to car serializer

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CarSerializer < ActiveModel::Serializer
-  attributes :id, :model, :year, :status, :daily_price, :owner, :address
+  attributes :id, :model, :year, :status, :daily_price, :latitude, :longitude, :owner, :address
 
   belongs_to :owner
 end
