577ffe274159b47aca1bc58b1b0cb379ee6dcd67
sopanda <hushchabogdan@gmail.com>
2019-02-14 00:26:42 +0200

    added role for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_13_213501) do
+ActiveRecord::Schema.define(version: 2019_02_13_222148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 2019_02_13_213501) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone_number"
+    t.string "role", default: "user"
   end
 
   add_foreign_key "bookings", "cars"
